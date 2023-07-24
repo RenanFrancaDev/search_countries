5d8d48f4c4ea35a7d04d24db6aac3cae352011da
Renan Franca <renanfranca.dev@gmail.com>
2023-07-23 21:40:35 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+document.getElementById('card').style.setProperty('display', 'none', 'important')
+
+
 function procurar(){
-    console.log('foi')
+    document.getElementById('card').style.setProperty('display', 'block', 'important')
     let pais = document.getElementById('pais').value
     let url = `https://restcountries.com/v3.1/name/${pais}?fullText=true`
 
     fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data[0]))
+    .then((data) => {
 
+    let bandeira = document.getElementById('bandeira');
+    let nome = document.getElementById('nome');
+    let capital = document.getElementById('capital');
+    let pop = document.getElementById('pop');
+    let cont = document.getElementById('cont');
+
+    
+    nome_pais.innerHTML = pais;
+    capital.innerHTML = data[0].capital[0];
+    pop.innerHTML = data[0].population;
+    cont.innerHTML = data[0].continents[0]
+    bandeira.src = data[0].flags.svg
+    })
 }
